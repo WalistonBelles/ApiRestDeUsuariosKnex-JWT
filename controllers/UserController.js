cd415c954ee79aca093bf989937c91c114333550
Waliston Belles <walistonbelles@bol.com.br>
2021-05-03 21:05:09 -0300

    Adicionado métodos de procurar usuário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,7 +3,20 @@ const User = require('../models/User');
 class UserController {
     
     async index(req, res) {
+        var users = await User.findAll();
+        res.json(users);
+    }
 
+    async findUser(req, res){
+        var id = req.params.id;
+        var user = await User.findById(id);
+        if (user == undefined){
+            res.status(404);
+            res.json({})
+        }else {
+            res.status(200);
+            res.json(user);
+        }
     }
 
     async create(req, res) {
--- models/User.js
+++ models/User.js
@@ -3,6 +3,31 @@ const bcrypt = require('bcrypt');
 
 class User {
 
+    async findAll(){
+        try{
+            var result = await knex.select(['id', 'email', 'name', 'role']).table('users');
+            return result;
+        }catch(err){
+            console.log(err);
+            return [];
+        }
+    }
+
+    async findById(id){
+        try{
+            var result = await knex.select(['id', 'email', 'name', 'role']).where({id: id}).table('users');
+            if(result.length > 0){
+                return result[0];
+            }
+            else {
+                return undefined;
+            }
+        }catch(err){
+            console.log(err);
+            return undefined;
+        }
+    }
+
     async new(email, password, name){
         try {
             var hash = await bcrypt.hash(password, 10);
